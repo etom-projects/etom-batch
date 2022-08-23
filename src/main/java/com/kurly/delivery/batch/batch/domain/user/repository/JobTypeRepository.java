5b9f5b38102387ede9cfd79dc892536147a56ff4
rio <dldydtjs2965@github.com>
2022-08-24 07:02:20 +0900

    feat: query 수정

--- src/main/java/com/kurly/delivery/batch/batch/domain/user/repository/JobTypeRepository.java
+++ src/main/java/com/kurly/delivery/batch/batch/domain/user/repository/JobTypeRepository.java
@@ -7,14 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 public interface JobTypeRepository extends JpaRepository<JobType, Long> {
 
     @Query(value = "from JobType jobType " +
-            "where jobType.startTime = '00:00:00' and jobType.finishTime = '03:00:00'")
+            "where jobType.id = 1")
     JobType findFirstTimeJob();
 
     @Query(value = "from JobType jobType " +
-            "where jobType.startTime = '02:00:00' and jobType.finishTime = '07:00:00'")
+            "where jobType.id = 2")
     JobType findSecondTimeJob();
 
     @Query(value = "from JobType jobType " +
-            "where jobType.startTime = '04:00:00' and jobType.finishTime = '07:00:00'")
+            "where jobType.id = 3")
     JobType findThirdTimeJob();
 }
